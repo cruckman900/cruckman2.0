e18624380fbab0709f40cd06ebd11fb71eadeaa2
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-04-14 13:25:30 -0400

    commented out linkedin link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div>
                 <a href="https://github.com/cruckman900" target="_blank"><i className='fa-brands fa-github'></i></a>
                 <a href="https://lineardescent.artstation.com" target="_blank"><i className='fa-brands fa-artstation'></i></a>
-                <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank"><i className='fa-brands fa-linkedin'></i></a>
+                {/* <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank"><i className='fa-brands fa-linkedin'></i></a> */}
                 <a href="https://www.facebook.com/christopher.ruckman" target="_blank"><i className='fa-brands fa-facebook'></i></a>
                 <a href="https://twitter.com/LinearDescent" target="_blank"><i className='fa-brands fa-twitter'></i></a>
             </div>
