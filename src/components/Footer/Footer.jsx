258373d7c2ebb517de593ac5875cdd7649b2fef8
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-04-18 19:45:50 -0400

    changed values for some colors and added alt tags for sm icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,11 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <div>
-                <a href="https://github.com/cruckman900" target="_blank"><i className='fa-brands fa-github'></i></a>
-                <a href="https://lineardescent.artstation.com" target="_blank"><i className='fa-brands fa-artstation'></i></a>
-                {/* <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank"><i className='fa-brands fa-linkedin'></i></a> */}
-                <a href="#" target="_blank"><i className='fa-brands fa-linkedin'></i></a>
-                <a href="https://www.facebook.com/christopher.ruckman" target="_blank"><i className='fa-brands fa-facebook'></i></a>
-                <a href="https://twitter.com/LinearDescent" target="_blank"><i className='fa-brands fa-twitter'></i></a>
+                <a href="https://github.com/cruckman900" target="_blank" alt="Github"><i className='fa-brands fa-github'></i></a>
+                <a href="https://lineardescent.artstation.com" target="_blank" alt="Artstation"><i className='fa-brands fa-artstation'></i></a>
+                <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank" alt="LinkedIn"><i className='fa-brands fa-linkedin'></i></a>
+                <a href="https://www.facebook.com/christopher.ruckman" target="_blank" alt="Facebook"><i className='fa-brands fa-facebook'></i></a>
+                <a href="https://twitter.com/LinearDescent" target="_blank"><i className='fa-brands fa-twitter' alt="Twitter"></i></a>
             </div>
             <div>
                 <i>&copy;{new Date().getFullYear()} cruckman. all rights reserved.</i> 
