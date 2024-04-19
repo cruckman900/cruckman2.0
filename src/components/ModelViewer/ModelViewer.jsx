838755ca9b49f35f7bc49955cbf8c383aff3bff8
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-04-19 04:36:02 -0400

    added overlay for canvas and put instructions inside - over tope of the canvas

--- src/components/ModelViewer/ModelViewer.jsx
+++ src/components/ModelViewer/ModelViewer.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/no-unknown-property */
+import { useState } from 'react';
 import { Suspense } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import { Canvas, useLoader } from '@react-three/fiber';
@@ -13,9 +14,20 @@ function Model({ url }) {
 }
 
 export default function ModelViewer({ url }) {
+    const [showInstructions, setShowInstructions] = useState(true);
+
+    function handleMouseEvent() {
+        setShowInstructions(!showInstructions);
+    }
+
     return (
         <div className='modelViewer'>
-            <Canvas camera={{fov: 75, near: 0.1, far: 1000, position: [0, 0, 5]}}>
+            <Canvas
+                className='modelViewer-canvas'
+                camera={{fov: 75, near: 0.1, far: 1000, position: [0, 0, 5]}}
+                onMouseOver={handleMouseEvent} onMouseOut={handleMouseEvent}
+                onTouchStart={handleMouseEvent} onTouchEnd={handleMouseEvent}
+            >
                 <directionalLight position={[10, 10, 10]} intensity={5} />
                 <directionalLight position={[-10, -10, -10]} intensity={5} />
                 <OrbitControls />
@@ -23,17 +35,39 @@ export default function ModelViewer({ url }) {
                     <Model url={ url } />
                 </Suspense>
             </Canvas>
-            <div className='modelViewer-commands'>
-                <span className='modelViewer-commands-command'>pan:</span>
-                <span className='modelViewer-commands-action'>right mouse button</span>
-            </div>
-            <div className='modelViewer-commands'>
-                <div className='modelViewer-commands-command'>rotate:</div>
-                <span className='modelViewer-commands-action'>left mouse button</span></div>
-            <div className='modelViewer-commands'>
-                <span className='modelViewer-commands-command'>zoom:</span>
-                <span className='modelViewer-commands-action'>middle mouse button/scroll wheel</span>
-            </div>
+            {
+                showInstructions &&
+                    <div className="modelViewer-overlay">
+                        <div className='modelViewer-commands-heading'>
+                            <span className='modelViewer-commands-heading'>Keyboard / Mouse</span>
+                        </div>
+                        <div className='modelViewer-commands'>
+                            <span className='modelViewer-commands-command'>pan:</span>
+                            <span className='modelViewer-commands-action'>right mouse button</span>
+                        </div>
+                        <div className='modelViewer-commands'>
+                            <div className='modelViewer-commands-command'>rotate:</div>
+                            <span className='modelViewer-commands-action'>left mouse button</span></div>
+                        <div className='modelViewer-commands'>
+                            <span className='modelViewer-commands-command'>zoom:</span>
+                            <span className='modelViewer-commands-action'>middle mouse button/scroll wheel</span>
+                        </div>
+                        <div className='modelViewer-commands-heading'>
+                            <span className='modelViewer-commands-heading'>Touch</span>
+                        </div>
+                        <div className='modelViewer-commands'>
+                            <span className='modelViewer-commands-command'>pan:</span>
+                            <span className='modelViewer-commands-action'>two finger - drag</span>
+                        </div>
+                        <div className='modelViewer-commands'>
+                            <div className='modelViewer-commands-command'>rotate:</div>
+                            <span className='modelViewer-commands-action'>single finger - drag</span></div>
+                        <div className='modelViewer-commands'>
+                            <span className='modelViewer-commands-command'>zoom:</span>
+                            <span className='modelViewer-commands-action'>two finger - pinch</span>
+                        </div>
+                    </div>
+            }
         </div>
     );
 }
