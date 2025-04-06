cd1e37fcdef49d8023c7a831aedab4cc100f949c
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-06 05:29:28 -0400

    changed twitter icon/link and added discord

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,11 +8,12 @@ export default function Footer() {
                 <a href="https://lineardescent.artstation.com" target="_blank" alt="Artstation"><i className='fa-brands fa-artstation'></i></a>
                 <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank" alt="LinkedIn"><i className='fa-brands fa-linkedin'></i></a>
                 <a href="https://www.facebook.com/christopher.ruckman" target="_blank" alt="Facebook"><i className='fa-brands fa-facebook'></i></a>
-                <a href="https://twitter.com/LinearDescent" target="_blank"><i className='fa-brands fa-twitter' alt="Twitter"></i></a>
+                <a href="https://x.com/LinearDescent" target="_blank"><i className='fa-brands fa-x-twitter' alt="X (Twitter)"></i></a>
+                <a href="https://discordapp.com/users/750463736300699671" target="_blank"><i className='fa-brands fa-discord' alt="Discord"></i></a>
             </div>
             <div>
                 <i>&copy;{new Date().getFullYear()} cruckman. all rights reserved.</i> 
             </div>
         </footer>
     );
-}
+} //750463736300699671
