b85afa1400c8ce5e14f35e6d2c820ad13a06da46
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-08 19:24:47 -0400

    added some text to HOME

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,15 +10,15 @@ export default function Home() {
                 <div className='home-div'>
                     I am currently developing this new site because the old one is pretty messy. I am hoping to have it more presentable soon. 
                     Some elements of the site may be hidden on mobile devices.
-                    My old site content is here if you want to take a look: 
-                    <div className='home-div-centered-link'>
-                        <a href='https://old.cruckman.com' target='_blank'>old cruckman.com website</a>
-                    </div>
+                    My old site content is here if you want to <a href='https://old.cruckman.com' target='_blank'>take a look here</a>. I had
+                    fun building it, but it isn&apos;t as carefully put together as this one. I&apos;ve learned quite a bit about modern CSS in the last 
+                    few years.
                 </div>
-                <div className='home-div'>
+                <div className='home-div large-output'>
                     <span><a href='https://chatterboxsm.com' target='_blank'>chatterboxsm</a></span><span id='chatterbox-text'> (the button / panel
                     to the right)</span> is another one of my creations. It&apos;s not fully functional yet, but just like everything else I work on in my
-                    spare time, it is not abandoned; it&apos;s just waiting for its turn in the rotation.
+                    spare time, it is not abandoned; it&apos;s just waiting for its turn in the rotation. I plan to redo it in NextJS and moving it to a 
+                    free hosting site until/unless it gains some kind of popularity.
                 </div>
             </Section>
         </DefaultPage>
