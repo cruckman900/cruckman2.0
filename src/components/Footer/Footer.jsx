5f87c28fa48d20fe5c66208c10a5a2a857fc69c3
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-04-30 00:58:04 -0400

    hide link buttons in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import './Footer.scss';
 export default function Footer() {
     return (
         <footer className='footer'>
-            <div>
+            {/* <div>
                 <a href="https://github.com/cruckman900" target="_blank" alt="Github"><i className='fa-brands fa-github'></i></a>
                 <a href="https://lineardescent.artstation.com" target="_blank" alt="Artstation"><i className='fa-brands fa-artstation'></i></a>
                 <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank" alt="LinkedIn"><i className='fa-brands fa-linkedin'></i></a>
                 <a href="https://www.facebook.com/christopher.ruckman" target="_blank" alt="Facebook"><i className='fa-brands fa-facebook'></i></a>
                 <a href="https://x.com/LinearDescent" target="_blank"><i className='fa-brands fa-x-twitter' alt="X (Twitter)"></i></a>
                 <a href="https://discordapp.com/users/750463736300699671" target="_blank"><i className='fa-brands fa-discord' alt="Discord"></i></a>
-            </div>
+            </div> */}
             <div>
                 <i>&copy;{new Date().getFullYear()} cruckman. all rights reserved.</i> 
             </div>
