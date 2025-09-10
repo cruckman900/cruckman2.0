284517e57c88af3cc86573ce03777da9854bf306
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-09-10 19:57:21 -0400

    disabling links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
             <div>
                 <a href="https://github.com/cruckman900" target="_blank" alt="Github"><i className='fa-brands fa-github'></i></a>
                 <a href="https://lineardescent.artstation.com" target="_blank" alt="Artstation"><i className='fa-brands fa-artstation'></i></a>
-                <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank" alt="LinkedIn"><i className='fa-brands fa-linkedin'></i></a>
+                {/* <a href="https://www.linkedin.com/in/christopher-ruckman-619318215" target="_blank" alt="LinkedIn"><i className='fa-brands fa-linkedin'></i></a>
                 <a href="https://www.facebook.com/christopher.ruckman" target="_blank" alt="Facebook"><i className='fa-brands fa-facebook'></i></a>
                 <a href="https://x.com/LinearDescent" target="_blank"><i className='fa-brands fa-x-twitter' alt="X (Twitter)"></i></a>
-                <a href="https://discordapp.com/users/750463736300699671" target="_blank"><i className='fa-brands fa-discord' alt="Discord"></i></a>
+                <a href="https://discordapp.com/users/750463736300699671" target="_blank"><i className='fa-brands fa-discord' alt="Discord"></i></a> */}
                 <a href="mailto:cruckman900@gmail.com"><i className='fa-solid fa-envelope'></i></a>
             </div>
             <div>
